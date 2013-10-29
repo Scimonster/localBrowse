d61e614960ebf54da6d36a4d301d060462facfff
Scimonster <tehalmightyscimonster@gmail.com>
2013-10-29 12:10:57 +0200

    Info on files.

--- info.js
+++ info.js
@@ -1,6 +1,6 @@
 // info functions
 
-var fs = require('fs');
+var fs = require('fs-extra'), mmm = require('mmmagic'), mimeLazy = require('mime');
 
 exports.master = {
 	get: function(req, res){
@@ -39,4 +39,73 @@ exports.readable = function(req, res) {
 	canReadWrite(req.file,true,function(r){
 		res.send(r);
 	});
+};
+
+function info(file,cb){
+	var i = {name: file};
+	fs.exists(file,function(e){
+		i.exists = e;
+		if (!e) {
+			cb(i);
+			return;
+		}
+		canReadWrite(file,false,function(w){
+			i.writable = w;
+			finished();
+		});
+		canReadWrite(file,true,function(r){
+			i.readable = r;
+			fs.stat(file,function(e,s){
+				i.date = s.mtime.getTime()/1000;
+				i.perm = parseInt(parseInt(s.mode.toString(8),10).toString(10).substr(2),8);
+				i.type = s.isDirectory()?'directory':'';
+				if (!i.type) { // is file
+					var magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE);
+					magic.detectFile(file,function(m_e,m_type){
+						if (m_e) { // Magic error, use lazy checking
+							i.type = mimeLazy.lookup(file);
+						} else {
+							i.type = m_type;
+						}
+						finished();
+					});
+					i.size = s.size;
+				} else {
+					fs.readdir(file,function(d_e,d){
+						i.size = d.length;
+						finished();
+					});
+				}
+				finished();
+			});
+			fs.lstat(file,function(e,s){
+				i.isLink = s.isSymbolicLink();
+				if (i.isLink) {
+					fs.readlink(file,function(e,l){
+						i.link = l;
+						finished();
+					});
+				} else {
+					finished();
+				}
+			});
+		});
+	});
+	function finished(){
+		if (
+			typeof i.writable !== 'undefined' &&
+			typeof i.readable !== 'undefined' &&
+			typeof i.date !== 'undefined' &&
+			typeof i.size !== 'undefined' &&
+			typeof i.date !== 'undefined' &&
+			typeof i.perm !== 'undefined' &&
+			typeof i.type !== 'undefined' && i.type !== '' &&
+			typeof i.isLink !== 'undefined' && (!i.isLink || typeof i.link !== 'undefined')) {
+			cb(i);
+		}
+	}
+}
+
+exports.info = function(req, res, content) {
+	info(req.file,function(i){res.send(i)});
 };
