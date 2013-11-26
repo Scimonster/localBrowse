67199a774cd5c34dc2f1fc416b5361f18d2fe9ab
Scimonster <tehalmightyscimonster@gmail.com>
2013-11-26 22:18:27 +0200

    Add JSDoc.

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ A file manager+editor to run in the browser with Node.js
 
 Is being ported to Node from the [PHP version](https://sourceforge.net/projects/localbrowse/).
 
-Currently supports most stuff. Not search.
+Currently supports everything the PHP version did. Also has some improvements.
+
+Feel free to submit a pull request, or make some issues for me to fix.
--- search.js
+++ search.js
@@ -1,5 +1,17 @@
-// Search
+/**
+ * @file Search for a term
+ * @author Scimonster
+ * @license {@link LICENSE} (MIT)
+ * @module search
+ * @requre child_process
+ * @require ./info
+ */
 var spawn = require('child_process').spawn, info = require('./info');
+/**
+ * Run the requests
+ * @param {Object} req Express request object
+ * @param {Object} res Express response object
+ */
 exports.search = function(req, res) {
 	var cwd = req.body.cwd?decodeURIComponent(req.body.cwd):process.env.HOME,
 	find = spawn('find',
