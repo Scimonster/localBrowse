bfd980b45afef9079d21f4500d835d199063fcb3
Scimonster <tehalmightyscimonster@gmail.com>
2013-12-15 15:41:13 +0200

    Open with system program.

---
+++ programs/system/index.js
@@ -0,0 +1,27 @@
+/**
+ * @file Program -- Open with system program
+ * @author Scimonster
+ * @license {@link LICENSE} (AGPL)
+ */
+var path = require('path'), spawn = require('child_process').spawn;
+var name = exports.modName = path.basename(__dirname);
+exports.html = function(file, cb) {
+	spawn('xdg-open', [file.path]);
+	cb('');
+};
+
+exports.messages = {en: {}};
+exports.messages.en["program-"+name] = "system program";
+
+exports.mimetypes = [/.*/];
+
+exports.desc = 'Open the file with the system\'s default program';
+exports.name = 'System program';
+
+exports.noShow = true;
+
+exports.routes = {};
+
+exports.buttons = function(f, cb) {
+	cb([]);
+};
