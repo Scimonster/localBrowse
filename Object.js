17cf14ab1f3f6115b16c96cdf19fd1193217c2ed
Scimonster <tehalmightyscimonster@gmail.com>
2014-01-01 21:47:08 +0200

    Object.js: Extend filter API to support shortcuts Extend unique to support plain objects as well as arrays Extend filter to return arrays and strings

--- Object.js
+++ Object.js
@@ -44,23 +44,56 @@ module.exports = {
 	},
 
 	'filter': function(self, fun, thisp) {
-		if (self == null || typeof fun != 'function') {
-			throw new TypeError();
+		if (self == null) {
+			throw new TypeError('self is null');
+		}
+		switch (typeof fun) {
+			case 'function':
+				break; // do nothing
+			case 'string':
+			case 'number':
+				return self[fun]; // str/num is just the property
+			case 'boolean': // boolean shortcuts to filter only truthy/falsy values
+				if (fun) {
+					fun = function(v){return v};
+				} else {
+					fun = function(v){return !v};
+				}
+				break;
+			case 'object':
+				var funOrig = fun; // save it
+				if (fun instanceof RegExp) { // test the val against the regex
+					fun = function(v){return funOrig.test(v)};
+					break;
+				} else if (Array.isArray(fun)) { // keep these keys
+					fun = function(v,k){return funOrig.indexOf(k)>-1};
+					break;
+				}
+			default:
+				throw new TypeError('fun is not a supported type');
 		}
 		var res = {};
 		var t = Object(self);
 		for (var i in t) {
 			if (t.hasOwnProperty(i)) {
 				var val = t[i]; // in case fun mutates it
-				console.log(i)
-				console.log(val)
 				if (fun.call(thisp, val, i, t)) {
-					console.log(true)
 					// define property on res in the same manner as it was originally defined
 					var props = Object.getOwnPropertyDescriptor(t, i);
 					props.value = val;
 					Object.defineProperty(res, i, props);
 				}
+			}
+		}
+		if (Array.isArray(self) || typeof self == 'string') { // came as an array, return an array
+			var arr = [];
+			for (var i in res) {
+				arr[Number(i)] = res[i];
+			}
+			res = arr.filter(function(v){return v}); // for...in isn't guaranteed to give any meaningful order
+			// can't use obj.filter(arr,true) here because that would infitely recurse
+			if (typeof self == 'string') {
+				res = res.join('');
 			}
 		}
 		return res;
@@ -100,6 +133,9 @@ module.exports = {
 		if (self == null) {
 			throw new TypeError();
 		}
+		if (typeof self == 'string' || Array.isArray(self) && typeof searchElement != 'function') {
+			return self.indexOf(searchElement);
+		}
 		if (loose && typeof searchElement == 'object') {
 			searchElement = JSON.stringify(searchElement);
 		}
@@ -129,9 +165,8 @@ module.exports = {
 		if (self == null) {
 			throw new TypeError();
 		}
-		return Array.prototype.filter.call(self, function(val, i, arr) {
-			// force using Array's filter even for strings or other array-likes
-			return arr.valueOf().indexOf(val) == i; // only keep first occurrence of anything
+		return module.exports.filter(self, function(val, i, obj) {
+			return module.exports.indexOf(obj, val) == i; // only keep first occurrence of anything
 		});
 	}
 
