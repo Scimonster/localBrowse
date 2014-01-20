6a2f6546b00c4f51f9271302137b7702f8e0d932
Scimonster <tehalmightyscimonster@gmail.com>
2014-01-20 16:17:08 +0200

    Add JSONfn to both server and client. https://github.com/vkiryukhin/jsonfn

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -25,3 +25,6 @@ var crit={typeOf:function(a){var b=typeof a;"object"===b&&(a?a instanceof Array&
 
 // http://stackoverflow.com/questions/1853223/check-if-object-is-a-jquery-object/12407361
 $.isJQuery=function(e){return e instanceof jQuery};
+
+// https://github.com/vkiryukhin/jsonfn
+!function(exports){"use strict";exports.stringify=function(e){return JSON.stringify(e,function(e,t){return t instanceof Function||"function"==typeof t?t.toString():t instanceof RegExp?"_PxEgEr_"+t:t})},exports.parse=function(str,date2obj){var iso8061=date2obj?/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/:!1;return JSON.parse(str,function(key,value){var prefix;return"string"!=typeof value?value:value.length<8?value:(prefix=value.substring(0,8),iso8061&&value.match(iso8061)?new Date(value):"function"===prefix?eval("("+value+")"):"_PxEgEr_"===prefix?eval(value.slice(8)):value)})},exports.clone=function(e,t){return exports.parse(exports.stringify(e),t)}}("undefined"==typeof exports?window.JSONfn={}:exports);
